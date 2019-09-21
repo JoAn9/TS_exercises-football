56d04c1f194530b912db2248965e4ffb7d457634
JoAn <joannacalinska@gmail.com>
2019-09-21 22:22:01 +0200

    move refactoring based on inheritance to other dir

---
+++ src/inheritance/index.ts
@@ -0,0 +1,19 @@
+import { MatchReader } from './MatchReader';
+import { MatchResults } from '../MatchResults';
+
+const reader = new MatchReader('football.csv');
+reader.read();
+
+const firstItem = reader.data[0];
+console.log(firstItem);
+
+let manUnitedWins = 0;
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Manchester United wins ${manUnitedWins} games`);
