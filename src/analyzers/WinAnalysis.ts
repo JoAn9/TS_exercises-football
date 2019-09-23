9393e12c75fcee3295b827e42255c4b36c550b45
JoAn <joannacalinska@gmail.com>
2019-09-23 20:47:46 +0200

    composition with summary class & interfaces

---
+++ src/analyzers/WinAnalysis.ts
@@ -0,0 +1,23 @@
+import { Analyzer } from '../Summary';
+import { MatchData } from '../MatchData';
+import { MatchResults } from '../MatchResults';
+
+export class WinAnalysis implements Analyzer {
+  constructor(public teamName: string) {}
+
+  run(matches: MatchData[]): string {
+    let wins = 0;
+    for (let match of matches) {
+      if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
+        wins++;
+      } else if (
+        match[2] === 'Man United' &&
+        match[5] === MatchResults.AwayWin
+      ) {
+        wins++;
+      }
+    }
+
+    return `Team ${this.teamName} wins ${wins} games`;
+  }
+}
